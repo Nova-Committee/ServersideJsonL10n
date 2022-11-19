3fac45e432436d0e912f978abef26e010281c515
MikhailTapio <mikhailtapio13468@163.com>
2022-11-20 00:42:16 +0800

    Get the value from en_us.json if the key is missing from the defined lang file

--- src/main/java/committee/nova/sjl10n/L10nUtilitiesJ.java
+++ src/main/java/committee/nova/sjl10n/L10nUtilitiesJ.java
@@ -11,7 +11,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
-import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.regex.Pattern;
 
@@ -25,43 +24,53 @@ public class L10nUtilitiesJ {
     private static final Logger LOGGER = LogManager.getLogger();
 
     private static JsonText create(String modId, String langId) {
-        ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
-        Objects.requireNonNull(builder);
-        BiConsumer<String, String> biConsumer = builder::put;
+        final ImmutableMap.Builder<String, String> builderDefault = ImmutableMap.builder();
+        final ImmutableMap.Builder<String, String> builder = ImmutableMap.builder();
+        final BiConsumer<String, String> consumerDefault = builderDefault::put;
+        final BiConsumer<String, String> consumer = builder::put;
         final String resourceFString = "/assets/%s/lang/%s.json";
         final String resourceLocation = String.format(resourceFString, modId, langId);
         try {
+            final InputStream inputStreamDefault = JsonText.class.getResourceAsStream(String.format(resourceFString, modId, DEFAULT_LANGUAGE));
             InputStream inputStream = JsonText.class.getResourceAsStream(resourceLocation);
             if (inputStream == null) {
                 LOGGER.info(String.format("No %s lang file for the language '%s' found. Make it to 'en_us' by default.", modId, langId));
-                inputStream = JsonText.class.getResourceAsStream(String.format(resourceFString, modId, DEFAULT_LANGUAGE));
+                inputStream = inputStreamDefault;
             }
-
             try {
-                load(inputStream, biConsumer);
-            } catch (Throwable var7) {
+                load(inputStreamDefault, consumerDefault);
+                load(inputStream, consumer);
+            } catch (Throwable var8) {
+                if (inputStreamDefault != null) {
+                    try {
+                        inputStreamDefault.close();
+                    } catch (Throwable var6) {
+                        var8.addSuppressed(var6);
+                    }
+                }
                 if (inputStream != null) {
                     try {
                         inputStream.close();
-                    } catch (Throwable var6) {
-                        var7.addSuppressed(var6);
+                    } catch (Throwable var7) {
+                        var8.addSuppressed(var7);
                     }
                 }
 
-                throw var7;
+                throw var8;
             }
-
-            if (inputStream != null) {
-                inputStream.close();
-            }
-        } catch (JsonParseException | IOException var8) {
+            if (inputStreamDefault != null) inputStreamDefault.close();
+            if (inputStream != null) inputStream.close();
+        } catch (JsonParseException | IOException | NullPointerException var8) {
             LOGGER.error("Couldn't read strings from {}", resourceLocation, var8);
         }
 
+        final Map<String, String> mapDefault = builderDefault.build();
         final Map<String, String> map = builder.build();
+
         return new JsonText() {
             public String get(String key) {
-                return map.getOrDefault(key, key);
+                final String value = map.get(key);
+                return value != null ? value : mapDefault.getOrDefault(key, key);
             }
         };
     }
