0fa557cf37aeadec52dec3dc4328c66eeb8f2190
MikhailTapio <mikhailtapio13468@163.com>
2022-11-20 00:30:29 +0800

    Get the value from en_us.json if the key is missing from the defined lang file

--- src/main/scala/committee/nova/sjl10n/L10nUtilities.scala
+++ src/main/scala/committee/nova/sjl10n/L10nUtilities.scala
@@ -18,27 +18,33 @@ object L10nUtilities {
   final val logger = LogManager.getLogger
 
   def create(modId: String, lang: String): JsonText = {
+    val builderDefault = ImmutableMap.builder[String, String]()
     val builder = ImmutableMap.builder[String, String]()
+    val consumerDefault = (s1: String, s2: String) => builderDefault.put(s1, s2)
     val consumer = (s1: String, s2: String) => builder.put(s1, s2)
     val resourceLocation = s"/assets/$modId/lang/$lang.json"
     try {
+      val streamDefault = classOf[JsonText].getResourceAsStream(s"/assets/$modId/lang/$defaultLang.json")
       var stream = classOf[JsonText].getResourceAsStream(resourceLocation)
       if (stream == null) {
         logger.info(s"No lang file for current language '$lang' found. Defaulted it to $defaultLang")
-        stream = classOf[JsonText].getResourceAsStream(s"/assets/$modId/lang/$defaultLang.json")
+        stream = streamDefault
       }
       try {
+        load(streamDefault, consumerDefault)
         load(stream, consumer)
       } catch {
         case t: Throwable => throw t
       } finally {
+        streamDefault.close()
         stream.close()
       }
     } catch {
       case e@(_: JsonParseException | _: IOException) => logger.error(s"Couldn't read strings from $resourceLocation", e)
     }
     val map = builder.build()
-    JsonText(key => map.getOrDefault(key, key))
+    val defaultMap = builderDefault.build()
+    JsonText(key => map.getOrElse(key, defaultMap.getOrDefault(key, key)))
   }
 
 
